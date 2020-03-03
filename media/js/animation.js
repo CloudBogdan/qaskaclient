dc5e645ae02b6528796ec39c0586a12526e8067b
CloudBogdan <60233692+CloudBogdan@users.noreply.github.com>
2020-03-03 18:17:04 +0300

    Add new fich

--- media/js/animation.js
+++ media/js/animation.js
@@ -1,13 +1,90 @@
 var dt = 0;
 
+var words = [
+    "people",
+    "you",
+    "programmers",
+    "us",
+    "me",
+    "all"
+];
+var word_index = 0;
+
 function loop() {
     requestAnimationFrame(loop);
 
     dt += .1;
     
     $(".circle").each(i=> {
-        $(".circle").css({
-            transform: `translateY(${ Math.sin(dt / 3) * 10 }px)`
+        $(".circle").eq(i).css({
+            transform: `translateY(${ Math.sin(dt / 3 * Math.cos(i * 2)) * 10 }px)`
         });
     });
+
+
+    var jump = -Math.abs(Math.sin(dt)) * 10;
+    $(".jump").css({
+        transform: `translateY(${ jump }px)`
+    });
 } loop();
+
+const input = $(".input");
+$(window).on("click", ()=> {
+    if (input.is(":focus") || input.val() !== "") {
+        $(".input ~ .placeholder").css({
+            top: 0,
+            left: "15px",
+            "z-index": 6,
+            "font-size": "12px"
+        });
+    } else {
+        $(".input ~ .placeholder").css({
+            top: "",
+            left: "",
+            "z-index": "",
+            "font-size": ""
+        });
+    }
+}); 
+
+const item = $(".nav_list_item");
+const stroke = $(".nav .stroke");
+
+stroke.css({
+    width: $(".nav_list_item.active").width() + 1 + "px",
+    left: $(".nav_list_item.active").offset().left - 185 + "px"
+});
+
+$(window).on("mousemove", ()=> {
+    item.each(i=> {
+        item.eq(i).on("mouseover", ()=> {
+            stroke.css({
+                width: item.eq(i).width() + 1 + "px",
+                left: item.eq(i).offset().left - 185 + "px"
+            });
+        });
+        item.eq(i).on("mouseout", ()=> {
+            stroke.css({
+                width: $(".nav_list_item.active").width() + 1 + "px",
+                left: $(".nav_list_item.active").offset().left - 185 + "px"
+            });
+        });
+    });
+});
+
+const nav = $(".nav");
+$(window).on("wheel", e=> {
+    var deltaY = e.originalEvent.deltaY;
+    
+    if (nav.offset().top > 698) {
+        if (deltaY > 0) {
+            nav.css({
+                transform: "translateY(-100%)"
+            })
+        } else {
+            nav.css({
+                transform: ""
+            })
+        }
+    }
+});
---
+++ media/js/fade.js
@@ -0,0 +1,41 @@
+const fade = {
+    right: $(".fade.right"),
+    left: $(".fade.left"),
+    top: $(".fade.top"),
+    bottom: $(".fade.bottom"),
+    all: $(".fade")
+};
+
+const win = $(window);
+
+win.on("scroll", ()=> {
+    fade.all.each(i=> {
+        if (win.scrollTop() + win.height() - 200 > fade.all.eq(i).offset().top) {
+
+            fade.all.eq(i).css({
+                opacity: 1
+            });
+
+            fade.right.eq(i).css({
+                transform: "translateX(0)",
+            });
+            fade.top.eq(i).css({
+                transform: "translateY(0)",
+            });
+            fade.left.eq(i).css({
+                transform: "translateX(0)",
+            });
+            fade.bottom.eq(i).css({
+                transform: "translateY(0)",
+            });
+
+        } else {
+            
+            fade.all.eq(i).css({
+                transform: "",
+                opacity: ""
+            });
+
+        }
+    });
+});
